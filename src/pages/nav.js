0138089ad8c59e5b8ed2de11580974869cc009f2
DEdmonds22 <ddedmonds22@gmail.com>
2023-08-02 17:15:33 -0400

    completed basic nav components

--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,33 +1,46 @@
+import { useState } from "react"
 
 export default function Nav() {
+
+    const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+    const openMenu = () => {
+        setMenuIsOpen(prev => !prev)
+    }
+
     return (
         <header>
-            <div id="navBar">
-                <p>The Book Club</p>
-                <input type="text" placeholder="Choose Your Book!"></input>
-                <p>user logo</p>
-                <div className="hamburger">
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
+            <div>
+                <div id="navBar">
+                    <p>The Book Club</p>
+                    <input type="text" placeholder="Choose Your Book!"></input>
+                    <p>user logo</p>
+                    <div className="hamburger" onClick={openMenu}>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                        <span className="bar"></span>
+                    </div>
+            </div>
+                
+                <div className={`nav-menu-container ${menuIsOpen ? "active" : ""}`}>
+                    <ul className="nav-menu">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Profile - TBA</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Book Shelve</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Full Library</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Book Clubs - TBA</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Settings - TBA</a>
+                        </li>
+                    </ul>
                 </div>
-                <ul className="nav-menu">
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Profile - TBA</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Book Shelve</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Full Library</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Book Clubs - TBA</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Settings - TBA</a>
-                    </li>
-                </ul>
             </div>
         </header>
     )
